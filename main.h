24739a1767a75dcc62b60d5bd814bba50ad6f33d
jezmu <jezmuenterprises@gmail.com>
2022-04-16 05:00:41 -0700

    add print int and print decimal function

--- main.h
+++ main.h
@@ -15,6 +15,13 @@ typedef struct specifier
 {
 	char *c;
 	int (*f)(va_list);
-} op_t;
-
+} spec;
+int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
+int _putchar(char c);
+int print_percent(va_list args);
+int (*get_func(char x))(va_list args);
 #endif
